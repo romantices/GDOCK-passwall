2e9348ce8be0d2ef96273fab16ef34d3791f4ab0
lbear79 <59015542+lbear79@users.noreply.github.com>
2020-01-19 14:54:28 +0800

    Update diy-newifi3.sh

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -1,2 +1,8 @@
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+echo '修改banner'
+rm -rf package/base-files/files/etc/banner
+cp -f ../banner-newifi3 package/base-files/files/etc/banner
+
+rm -rf package/base-files/files/etc/banner
